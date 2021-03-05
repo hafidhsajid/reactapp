b75aa71389fe47fd6ccbde213ce9bac42b48355e
Hafidh Sajid <hafidhsajid@gmail.com>
2021-03-05 15:53:08 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h5>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </h5>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Google
         </a>
       </header>
     </div>
